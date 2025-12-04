3227e2c56a0903e945784f84d9135e2758f8b525
Thomas Teiten <teitenthomas@gmail.com>
2025-12-04 23:57:49 +0100

    Correction des erreurs lié aux imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './ressources/css/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
