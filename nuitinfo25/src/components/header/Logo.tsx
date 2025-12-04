a343edef55e03ec7ec7a6e541a386e874b52ad02
Thomas Teiten <teitenthomas@gmail.com>
2025-12-04 23:27:18 +0100

    Gestion du logo

--- nuitinfo25/src/components/header/Logo.tsx
+++ nuitinfo25/src/components/header/Logo.tsx
@@ -1,7 +1,9 @@
+import logo from '../../ressources/images/LogoRevive.png'
+
 const Logo = () => {
     return (
         <div>
-
+            <img src={logo} alt="Revive Logo"/>
         </div>
     );
 }
