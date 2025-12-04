e2e33c9eefbbe795c01ea329b06d76559f8696de
Whiplashhhh <vanbaelinghemwillem@gmail.com>
2025-12-04 23:43:59 +0100

    Add initial project structure with Header, Footer, Menu, and Logo components

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,11 @@
+import logo from '../../ressources/images/LogoRevive.png'
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={logo} alt="Revive Logo"/>
+        </div>
+    );
+}
+
+export default Logo
---
+++ src/components/header/Menu.tsx
@@ -0,0 +1,14 @@
+import '../../ressources/css/menu.css'
+
+const Menu = () => {
+  return (
+    <div>
+        <button className="menu-btn">
+            MENU <span className="icon">=</span>
+        </button>
+
+    </div>
+  );
+}
+
+export default Menu
