554f79de4146724ba824fb5541cfb90a0bf03925
Thomas Teiten <teitenthomas@gmail.com>
2025-12-05 03:43:35 +0100

    Modification slogan

--- src/components/header/Slogan/Slogan.tsx
+++ src/components/header/Slogan/Slogan.tsx
@@ -2,8 +2,12 @@ import img from '../../../ressources/images/SloganIMG.jpg'
 
 const Slogan = () => {
     return (
-    <div>
+    <div className="header-slogan">
         <img src={img} alt="Fond Slogan"/>
+        <div className="header-slogan-text">
+            Agir aujourd’hui pour un digital
+            plus juste et durable demain
+        </div>
     </div>
     );
 }
